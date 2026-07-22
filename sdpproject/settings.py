7947b4d6573fc229d18873087d620ec63b5fd161
khuntaaryan <khuntaaryan@gmail.com>
2026-07-22 23:16:26 +0530

    new commit

--- sdpproject/settings.py
+++ sdpproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@(-5vv65c%bpebbyq17td0acpu#69+^s5^ek%@iu3p5p82-twg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
 
 
 # Application definition
